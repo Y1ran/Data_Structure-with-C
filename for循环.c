4962c2dc4e4781f838ba65757c63d0fd8b46164e
hatwanghacker <deanyuton@gmail.com>
2017-11-19 20:41:49 +0800

    for循环

---
+++ for循环.c
@@ -0,0 +1,25 @@
+/*≤‚ ‘For—≠ª∑
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+
+unsigned char ch;
+
+//scanf("%d", &ch);
+
+//for(;ch != '\n';)
+do {
+continue;
+}
+while ((ch = getchar()) == ' ');
+
+printf("%c", ch);
+
+
+
+return 0;
+
+}
